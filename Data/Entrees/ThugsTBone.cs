b42590811c138a3a64dffbfc065c371366ea4db1
rob clancy <rotclanny@ksu.edu>
2020-09-03 13:57:35 -0700

    made ThugsTBone class public

--- Data/Entrees/ThugsTBone.cs
+++ Data/Entrees/ThugsTBone.cs
@@ -13,7 +13,7 @@ namespace BleakwindBuffet.Data.Entrees
     /// <summary>
     /// class that represents a steak
     /// </summary>
-    class ThugsTBone
+    public class ThugsTBone
     {
         /// <summary>
         /// Gets the price of the steak
